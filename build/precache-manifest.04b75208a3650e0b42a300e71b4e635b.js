583fc51245eb2d8506d20b568363a61d40d480c3
fobossalmeron <fobos.salmeron@gmail.com>
2018-11-04 23:57:08 +0100

    for gh pages

---
+++ build/precache-manifest.04b75208a3650e0b42a300e71b4e635b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2fca6a898012b721ac0a49162de64532",
+    "url": "/static/media/arrow.2fca6a89.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e7623e9bb1a13d1ddf66",
+    "url": "/static/js/main.e7623e9b.chunk.js"
+  },
+  {
+    "revision": "e5c91e899d10ed74aef0",
+    "url": "/static/js/1.e5c91e89.chunk.js"
+  },
+  {
+    "revision": "e7623e9bb1a13d1ddf66",
+    "url": "/static/css/main.1e892752.chunk.css"
+  },
+  {
+    "revision": "a25ae9215ea5b1996f8a2272fd771ad9",
+    "url": "/index.html"
+  }
+];
