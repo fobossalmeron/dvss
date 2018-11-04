6831dbe3cf51e4a238991a5a15408cfeb400a080
fobossalmeron <fobos.salmeron@gmail.com>
2018-11-05 00:09:57 +0100

    Updates

---
+++ precache-manifest.9df1de460b61eca5521924ea4d8367cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2fca6a898012b721ac0a49162de64532",
+    "url": "/dvss/static/media/arrow.2fca6a89.svg"
+  },
+  {
+    "revision": "7dafebfe2ad44a0db938",
+    "url": "/dvss/static/js/runtime~main.7dafebfe.js"
+  },
+  {
+    "revision": "07db4801bff245cc57b8",
+    "url": "/dvss/static/js/main.07db4801.chunk.js"
+  },
+  {
+    "revision": "96d3332e7a0c0869cd76",
+    "url": "/dvss/static/js/1.96d3332e.chunk.js"
+  },
+  {
+    "revision": "07db4801bff245cc57b8",
+    "url": "/dvss/static/css/main.1e892752.chunk.css"
+  },
+  {
+    "revision": "657e0725631ff8edf31e168dce85242e",
+    "url": "/dvss/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.04b75208a3650e0b42a300e71b4e635b.js"
+  "/dvss/precache-manifest.9df1de460b61eca5521924ea4d8367cf.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/dvss/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
