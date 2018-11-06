f0d7bfd5ba7c2d9217af97ac361361c23462e6ae
fobossalmeron <fobos.salmeron@gmail.com>
2018-11-07 00:18:26 +0100

    Updates

---
+++ precache-manifest.836128a02e5f89ad123e9fb6684ad097.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "1325585e2ca1b3665b1682cdb88c7835",
+    "url": "/dvss/static/media/equal.1325585e.svg"
+  },
+  {
+    "revision": "2fca6a898012b721ac0a49162de64532",
+    "url": "/dvss/static/media/arrow.2fca6a89.svg"
+  },
+  {
+    "revision": "7dafebfe2ad44a0db938",
+    "url": "/dvss/static/js/runtime~main.7dafebfe.js"
+  },
+  {
+    "revision": "c8e6d5d7c4170abb5d01",
+    "url": "/dvss/static/js/main.c8e6d5d7.chunk.js"
+  },
+  {
+    "revision": "9dd335e3d7fcad664f22",
+    "url": "/dvss/static/js/1.9dd335e3.chunk.js"
+  },
+  {
+    "revision": "c8e6d5d7c4170abb5d01",
+    "url": "/dvss/static/css/main.d47414d8.chunk.css"
+  },
+  {
+    "revision": "93381ed9e84db921a5fef74ca47ade40",
+    "url": "/dvss/index.html"
+  }
+];
